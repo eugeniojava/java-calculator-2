46d3efeed443421fee3651d077d04af18f47dc90
Eugênio Moreira <eugeniojava@outlook.com>
2022-06-19 18:40:05 -0300

    feat: change application main to Spring

--- src/main/java/com/eugeniojava/Main.java
+++ src/main/java/com/eugeniojava/Main.java
@@ -1,16 +1,11 @@
 package com.eugeniojava;
 
-import com.eugeniojava.operation.Operation;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        double a = Double.parseDouble(args[0]);
-        double b = Double.parseDouble(args[2]);
-        char operationSymbol = args[1].charAt(0);
-        Operation operation = OperationUtil.getOperations().get(String.valueOf(operationSymbol));
-        if (operation == null) {
-            throw new IllegalArgumentException("Unknown operator: " + operationSymbol);
-        }
-        System.out.println(operation.calculate(a, b));
+        SpringApplication.run(Main.class, args);
     }
 }
